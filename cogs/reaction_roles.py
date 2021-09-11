6df559d1864a445477c0c1d9890ccaf71d1123cc
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2021-09-11 19:43:52 +0200

    Added reaction roles for games

--- cogs/reaction_roles.py
+++ cogs/reaction_roles.py
@@ -1,8 +1,10 @@
 import discord
+from discord.embeds import Embed
 from discord.ext import commands
 import configparser
 
-reaction_message_id = 886181942544982087
+kurswahl_reaction_message_id = 886181942544982087
+gamewahl_reaction_message_id = 886297996457820191
 
 config = configparser.ConfigParser()
 config.read('settings.cfg')
@@ -13,7 +15,7 @@ class reaction_roles(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        if payload.message_id == reaction_message_id:
+        if payload.message_id == kurswahl_reaction_message_id:
             if payload.emoji.name == '🇪🇸':
                 role_id = 884830841543467009
                 role = discord.utils.get(payload.member.guild.roles, id=role_id)
@@ -28,6 +30,23 @@ class reaction_roles(commands.Cog):
                 return await payload.member.add_roles(role)
             elif payload.emoji.name == '☁️':
                 role_id = 884830949580349451
+                role = discord.utils.get(payload.member.guild.roles, id=role_id)
+                return await payload.member.add_roles(role)
+        elif payload.message_id == gamewahl_reaction_message_id:
+            if payload.emoji.name == 'leagueoflegends':
+                role_id = 884827409843638382
+                role = discord.utils.get(payload.member.guild.roles, id=role_id)
+                return await payload.member.add_roles(role)
+            elif payload.emoji.name == 'Minecraft':
+                role_id = 884827761280180276
+                role = discord.utils.get(payload.member.guild.roles, id=role_id)
+                return await payload.member.add_roles(role)
+            elif payload.emoji.name == 'gta5':
+                role_id = 884827581713616988
+                role = discord.utils.get(payload.member.guild.roles, id=role_id)
+                return await payload.member.add_roles(role)
+            elif payload.emoji.name == 'cod':
+                role_id = 884827504559398962
                 role = discord.utils.get(payload.member.guild.roles, id=role_id)
                 return await payload.member.add_roles(role)
 
@@ -35,7 +54,7 @@ class reaction_roles(commands.Cog):
     async def on_raw_reaction_remove(self, payload):
         guild = self.bot.get_guild(payload.guild_id)
         member = await guild.fetch_member(payload.user_id)
-        if payload.message_id == reaction_message_id:
+        if payload.message_id == kurswahl_reaction_message_id:
             if payload.emoji.name == '🇪🇸':
                 role_id = 884830841543467009
                 role = discord.utils.get(guild.roles, id=role_id)
@@ -50,6 +69,23 @@ class reaction_roles(commands.Cog):
                 return await member.remove_roles(role)
             elif payload.emoji.name == '☁️':
                 role_id = 884830949580349451
+                role = discord.utils.get(guild.roles, id=role_id)
+                return await member.remove_roles(role)
+        elif payload.message_id == gamewahl_reaction_message_id:
+            if payload.emoji.name == 'leagueoflegends':
+                role_id = 884827409843638382
+                role = discord.utils.get(guild.roles, id=role_id)
+                return await member.remove_roles(role)
+            elif payload.emoji.name == 'Minecraft':
+                role_id = 884827761280180276
+                role = discord.utils.get(guild.roles, id=role_id)
+                return await member.remove_roles(role)
+            elif payload.emoji.name == 'gta5':
+                role_id = 884827581713616988
+                role = discord.utils.get(guild.roles, id=role_id)
+                return await member.remove_roles(role)
+            elif payload.emoji.name == 'cod':
+                role_id = 884827504559398962
                 role = discord.utils.get(guild.roles, id=role_id)
                 return await member.remove_roles(role)
     
@@ -62,6 +98,18 @@ class reaction_roles(commands.Cog):
         .add_field(name = 'Ethik', value = '☁️')
         )
         reactions = ['🇪🇸', '⛪', '✝️', '☁️']
+        for reaction in reactions:
+            await message.add_reaction(reaction)
+
+    @commands.command(name = 'gamewahl')
+    async def function_gaming_wahl(self, ctx):
+        message = await ctx.send(embed = Embed(title = 'Gamewahl', description = 'Erhalte Zugriff auf gamespezifische Channel.', colour = int(config.get('COLOUR_SETTINGS', 'standart'), base = 16))
+        .add_field(name = 'League of Legends', value = '<:leagueoflegends:886292690269003826>')
+        .add_field(name = 'Grand Theft Auto V', value = '<:gta5:886294793976709180>')
+        .add_field(name = 'Minecraft', value='<:Minecraft:886295237578879017>')
+        .add_field(name = 'Call of Duty', value='<:cod:886297203449147402>'))
+
+        reactions = ['<:leagueoflegends:886292690269003826>', '<:gta5:886294793976709180>', '<:Minecraft:886295237578879017>', '<:cod:886297203449147402>']
         for reaction in reactions:
             await message.add_reaction(reaction)
 
