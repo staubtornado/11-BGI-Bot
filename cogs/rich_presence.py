0700f17a91e69f8ab28561a153c59b40000b85dd
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2021-09-15 20:09:15 +0200

    Added rich presence showing prefix and version of the bot

---
+++ cogs/rich_presence.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands, tasks
+import configparser
+
+version = configparser.ConfigParser()
+version.read('version.cfg')
+
+config = configparser.ConfigParser()
+config.read('settings.cfg')
+
+class rich_presence(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.rich_presence_update.start()
+
+    def cog_unload(self):
+        self.rich_presence_update.cancel()
+
+    @tasks.loop(seconds = 20)
+    async def rich_presence_update(self):
+        await self.bot.wait_until_ready()
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name=f'/help | version: {version.get("BOT_VERSION", "latest_commit")}'))
+
+def setup(bot):
+    bot.add_cog(rich_presence(bot))
